8de90c9f4f8c82537b073ab0dbfea4b5cd56d610
Shubham Yadav <shubham.relieved@gmail.com>
2021-03-26 12:45:03 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Components } from 'react';
+// import './App.css';
+import Todo from './Components/Todo';
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <Todo />
+    </div>
+  );
+}
+
+export default App;
